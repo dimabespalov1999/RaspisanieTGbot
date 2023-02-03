90bcc0a27c8d45958aa9d13fda0a24fe4c5a5da7
bec <dimabesp4lov@yandex.ru>
2023-02-04 00:02:31 +0300

    исправлен пусть до бд

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-token_bot = "5819256207:AAGEgF83TzYErQHGFBmLwTsQd-bArvyutzAdededaA"
-path_db = "bot.db"
+token_bot = "5819256207:AAGEgF83TzYErQHGFBmLwTsQd-bArvyutzA"
+path_db = "./bot.db"
 admin_id = "624886634"
 start_upd_hour = 6
 start_upd_min = 10
